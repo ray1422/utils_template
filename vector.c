c362904aa361f045bf5cac3a2408062a0ad3e267
Ray1422 <30434835+ray1422@users.noreply.github.com>
2021-11-02 17:20:10 +0800

    edit vec
    #include <assert.h>
    #include <stdio.h>
    #include <stdlib.h>
    /**************** BEGIN VECTOR ****************/
    #ifndef VECTOR
    #define VECTOR
    #define vector(identifier) struct vector_##typename
    #define vector_register(type, identifier) \
        vector(identifier) {                  \
            int cap;                          \
            int len;                          \
            type* arr;                        \
        }
    #define vector_init(type, identifier) \
        (vector(identifier)) { .cap = 0, .len = 0, .arr = NULL }
    #define vector_destory(v, callback)                             \
        do {                                                        \
            for (int i = 0; i < v.len; i++) (callback)((v).arr[i]); \
            free((v).arr);                                          \
        } while (0)
    #define vector_push(v, val)                                                     \
        do {                                                                        \
            if ((v).cap < (v).len + 2) {                                            \
                (v).arr = realloc((v).arr, sizeof(*((v).arr)) * 2 * ((v).cap + 1)); \
                assert((v).arr != NULL);                                            \
                (v).cap = 2 * ((v).cap + 1);                                        \
            }                                                                       \
            (v).arr[(v).len++] = (val);                                             \
        } while (0)
    
    #endif
    /***************** END VECTOR *****************/
    
    #ifndef VECTOR_INT
    #define VECTOR_INT
    vector_register(int*, INT_PTR);
    #endif
    int main() {
        vector(int*) v = vector_init(int, INT_PTR);
        vector_push(v, malloc(4));
        vector_push(v, malloc(4));
        vector_push(v, malloc(4));
        vector_push(v, malloc(4));
        *v.arr[v.len - 2] = 19;
        printf("%d\n", *v.arr[v.len - 2]);
        vector_destory(v, free);
    }

--- vector.c
+++ vector.c
@@ -1,19 +1,22 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define vector(type) struct vector_##type
-#define vector_register(type) \
-                              \
-    vector(type) {            \
-        int cap;              \
-        int len;              \
-        type* arr;            \
+/**************** BEGIN VECTOR ****************/
+#ifndef VECTOR
+#define VECTOR
+#define vector(identifier) struct vector_##typename
+#define vector_register(type, identifier) \
+    vector(identifier) {                  \
+        int cap;                          \
+        int len;                          \
+        type* arr;                        \
     }
-#define vector_init(type) \
-    (vector(type)) { .cap = 0, .len = 0, .arr = NULL }
-#define vector_destory(v) \
-    do {                  \
-        free((v).arr);    \
+#define vector_init(type, identifier) \
+    (vector(identifier)) { .cap = 0, .len = 0, .arr = NULL }
+#define vector_destory(v, callback)                             \
+    do {                                                        \
+        for (int i = 0; i < v.len; i++) (callback)((v).arr[i]); \
+        free((v).arr);                                          \
     } while (0)
 #define vector_push(v, val)                                                     \
     do {                                                                        \
@@ -24,28 +27,21 @@
         }                                                                       \
         (v).arr[(v).len++] = (val);                                             \
     } while (0)
-typedef struct node {
-    struct node* parents;
-    struct node* children;
-} node;
 
-struct vector_head {
-    int cap;
-    int val;
-};
+#endif
+/***************** END VECTOR *****************/
+
 #ifndef VECTOR_INT
 #define VECTOR_INT
-vector_register(int);
+vector_register(int*, INT_PTR);
 #endif
 int main() {
-    vector(int) v = vector_init(int);
-    vector_push(v, 1);
-    vector_push(v, 1);
-    vector_push(v, 1);
-    vector_push(v, 1);
-    vector_push(v, 1);
-    vector_push(v, 199);
-    vector_push(v, 10);
-    printf("%d\n", v.arr[v.len - 2]);
-    vector_destory(v);
+    vector(int*) v = vector_init(int, INT_PTR);
+    vector_push(v, malloc(4));
+    vector_push(v, malloc(4));
+    vector_push(v, malloc(4));
+    vector_push(v, malloc(4));
+    *v.arr[v.len - 2] = 19;
+    printf("%d\n", *v.arr[v.len - 2]);
+    vector_destory(v, free);
 }
